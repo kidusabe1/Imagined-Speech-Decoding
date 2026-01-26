e4c78bf03e54934763115eb90d5c4aac804430ee
FAST Developer <user@fast.local>
2026-01-26 11:50:59 +0000

    deleted unnecessary files

--- BCIC2020Track3_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# if you only have a single GPU, you can run the following command
-# python3 BCIC2020Track3_train.py --gpu 0 --folds "0-15"
-
-python3 BCIC2020Track3_train.py --gpu 0 --folds "0-7"   &
-python3 BCIC2020Track3_train.py --gpu 1 --folds "7-15"  &
-wait
-
